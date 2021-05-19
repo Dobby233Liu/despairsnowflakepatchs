6ce9604b68a97d310fde4302cd6a813664fd4519
Liu Wenyuan <15816141883@163.com>
2021-05-19 08:33:52 -0500

    [skip ci] fix

--- bugfix_preset/patch.js
+++ bugfix_preset/patch.js
@@ -1,15 +1,16 @@
 {
 	"dependencies": [
 		"thpatch/aero",
-    "wobuffet3/visuals",
-    "32th-System/esc_r",
-    "pgj1997/doggo",
-    "rosenrose/bottom_enemy",
-    "ExpHP/anm_leak",
-    "ExpHP/continue",
-    "ExpHP/nopauseblur",
-    "ExpHP/sprite_death_fix",
-    "zero318/SpellcardLimit"
+		"wobuffet3/visuals",
+		"32th/esc_r",
+		"pgj1997/doggo",
+		"rosenrose/bottom_enemy",
+		"rosenrose/ufo_vivid",
+		"ExpHP/anm_leak",
+		"ExpHP/continue",
+		"ExpHP/nopauseblur",
+		"ExpHP/sprite_death_fix",
+		"zero318/SpellcardLimit"
 	],
 	"id": "bugfix_preset",
 	"servers": [
