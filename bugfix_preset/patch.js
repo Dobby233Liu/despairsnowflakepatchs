bfca3bc8c77dab4486a3b7ad7d420824f5144b6e
Liu Wenyuan <15816141883@163.com>
2021-05-19 08:28:43 -0500

    [skip ci]

---
+++ bugfix_preset/patch.js
@@ -0,0 +1,20 @@
+{
+	"dependencies": [
+		"thpatch/aero",
+    "wobuffet3/visuals",
+    "32th-System/esc_r",
+    "pgj1997/doggo",
+    "rosenrose/bottom_enemy",
+    "ExpHP/anm_leak",
+    "ExpHP/continue",
+    "ExpHP/nopauseblur",
+    "ExpHP/sprite_death_fix",
+    "zero318/SpellcardLimit"
+	],
+	"id": "bugfix_preset",
+	"servers": [
+		"https://raw.githubusercontent.com/Dobby233Liu/despairsnowflakepatchs/master/bugfix_preset/"
+	],
+	"title": "Bugfix patches collection",
+	"update": true
+}
