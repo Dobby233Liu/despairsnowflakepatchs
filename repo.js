060e263885ce147857c289512f9136d2c2727a81
Liu Wenyuan <15816141883@163.com>
2021-05-19 08:17:05 -0500

    [skip ci]

--- repo.js
+++ repo.js
@@ -2,11 +2,13 @@
 	"contact": "https://github.com/DespairSnowflake",
 	"id": "DespairSnowflake",
 	"patches": {
-		"gcym_alts": "alt music for guichuyinmad.",
-		"guichuyinmad": "Touhou Guichu Otomad (for simp.chn)"
+		"gcym_alts": "Alt music for guichuyinmad",
+		"guichuyinmad": "Touhou Guichu Otomad\nChinese otomad music mod for th17. Assets meant for Simplified Chinese\n    (move pending)",
+		"meiryo": "Force Meiryo",
+		"bugfix_preset": "Bugfix patches collection",
 	},
 	"servers": [
 		"https://raw.githubusercontent.com/Dobby233Liu/despairsnowflakepatchs/master/"
 	],
-	"title": "DespairSnowflake's patchs"
+	"title": "DespairSnowflake's patches"
 }
