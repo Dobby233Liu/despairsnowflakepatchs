59244dcda52ea8f848ab4688e397723e05505128
Liu Wenyuan <15816141883@163.com>
2021-05-19 08:50:36 -0500

    Create repo.js

--- repo.js
+++ repo.js
@@ -5,7 +5,7 @@
 		"gcym_alts": "Alt music for guichuyinmad",
 		"guichuyinmad": "Touhou Guichu Otomad\nChinese otomad music mod for th17. Assets meant for Simplified Chinese\n    (move pending)",
 		"meiryo": "Force Meiryo",
-		"bugfix_preset": "Bugfix patches collection",
+		"bugfix_preset": "Bugfix patches collection"
 	},
 	"servers": [
 		"https://raw.githubusercontent.com/Dobby233Liu/despairsnowflakepatchs/master/"
