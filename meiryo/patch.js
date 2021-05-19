c182dc865f4e97ba789c38c8d73b140e9d3a7c90
Liu Wenyuan <15816141883@163.com>
2021-05-19 08:39:58 -0500

    [skip ci]

---
+++ meiryo/patch.js
@@ -0,0 +1,11 @@
+{
+	"dependencies": [
+		"thpatch/base_tsa"
+	],
+	"id": "meiryo",
+	"servers": [
+		"https://raw.githubusercontent.com/Dobby233Liu/despairsnowflakepatchs/master/meiryo/"
+	],
+	"title": "Force Meiryo",
+	"update": true
+}
