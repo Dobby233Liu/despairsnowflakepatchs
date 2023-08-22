cedbf148ce53611efdf91dd8811b96904bff5e0d
Liu Wenyuan <15816141883@163.com>
2023-08-22 12:29:53 +0800

    a

--- the_deviants_meh_song/patch.js
+++ the_deviants_meh_song/patch.js
@@ -6,6 +6,6 @@
 	"servers": [
 		"https://raw.githubusercontent.com/Dobby233Liu/despairsnowflakepatchs/master/the_deviants_meh_song/"
 	],
-	"title": "Removes the vocal in The Deviants' Unobstructed Light ~ Kingdam of Nothingness. (th19_12) (and possibly more)",
+	"title": "Removes the vocal in The Unobstructed Light of Deviant People ~ Kingdom of Nothingness (th19_12) (and possibly more)",
 	"update": true
 }
